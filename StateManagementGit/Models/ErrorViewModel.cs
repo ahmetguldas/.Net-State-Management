925304837f7b998962d6ebdbb22b9c57fa65554b
ahmet güldaş <ahmet.guldas@hotmail.com>
2021-08-29 00:52:55 +0300

    Add project files.

---
+++ StateManagementGit/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StateManagementGit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
